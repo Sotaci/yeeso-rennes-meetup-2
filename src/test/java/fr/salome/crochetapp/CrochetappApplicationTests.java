8a2d3a529f472608b13fdd05119677d525fa9693
salomé jarnouen <salome.jarnouen.ext@idnow.io>
2026-01-17 18:15:37 +0100

    first commit

---
+++ src/test/java/fr/salome/crochetapp/CrochetappApplicationTests.java
@@ -0,0 +1,14 @@
+package fr.salome.crochetapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrochetappApplicationTests {
+/*
+	@Test
+	@Disabled
+	void contextLoads() {
+	}*/
+
+}
